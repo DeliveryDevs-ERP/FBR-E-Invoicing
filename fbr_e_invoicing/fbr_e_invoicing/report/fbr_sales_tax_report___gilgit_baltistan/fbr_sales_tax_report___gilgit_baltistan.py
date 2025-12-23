18b5b1d8327e39164c6eee293d8dba3a45c92237
bhavesh95863 <bhavesh.freelancer95863@gmail.com>
2025-12-23 16:24:55 +0500

    item fix

---
+++ fbr_e_invoicing/fbr_e_invoicing/report/fbr_sales_tax_report___gilgit_baltistan/fbr_sales_tax_report___gilgit_baltistan.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2025, osama.ahmed@deliverydevs.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+import frappe
+from fbr_e_invoicing.fbr_e_invoicing.report.report_utils import get_columns, get_data
+
+def execute(filters=None):
+    if not filters:
+        filters = {}
+    
+    # Force Sindh tax category
+    filters["tax_category"] = "GILGIT BALTISTAN"
+    
+    # Get columns and data with Sindh-specific handling
+    columns = get_columns(filters, province="GILGIT BALTISTAN")
+    data = get_data(filters, province="GILGIT BALTISTAN")
+    
+    return columns, data
