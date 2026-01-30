f4521879a161b2f88edfdd2144be41d0199dabe2
bhavesh95863 <bhavesh.freelancer95863@gmail.com>
2026-01-30 15:19:41 +0500

    Installation fix

--- fbr_e_invoicing/install.py
+++ fbr_e_invoicing/install.py
@@ -1,3 +1,5 @@
-from fbr_e_invoicing.utils import sync_hs_codes
+from fbr_e_invoicing.utils import sync_hs_codes, sync_provinces, create_fbr_sale_types
 def after_install():
+    create_fbr_sale_types()
     sync_hs_codes()
+    sync_provinces()
