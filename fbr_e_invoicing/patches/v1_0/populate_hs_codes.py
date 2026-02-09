a48b56b5d56f6565f090105ba480c92d52f3cfbd
OsamaASIdd <osama.siddiqui2017@gmail.com>
2026-02-09 18:38:13 +0500

    FBR sale type insertion patch

--- fbr_e_invoicing/patches/v1_0/populate_hs_codes.py
+++ fbr_e_invoicing/patches/v1_0/populate_hs_codes.py
@@ -1,8 +1,10 @@
 import frappe
-from fbr_e_invoicing.utils import sync_hs_codes
+from fbr_e_invoicing.utils import sync_hs_codes, sync_provinces, create_fbr_sale_types
 def execute():
     # 1. FORCE the schema update immediately
     # This adds the 'description' column to the database
     frappe.reload_doc("fbr_e_invoicing", "doctype", "hs_code")
     # 2. Now it is safe to insert data
     sync_hs_codes()
+    sync_provinces()
+    create_fbr_sale_types()
