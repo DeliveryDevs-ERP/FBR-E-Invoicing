cc4782a2bd5a484e12e504d2c00b4f86682bff0b
ali-raza-77 <ali.raza.ghafoor@gmail.com>
2026-02-25 03:13:32 +0500

    bug fixes

--- fbr_e_invoicing/fbr_e_invoicing/doctype/fbr_e_inv_setup/fbr_e_inv_setup.js
+++ fbr_e_invoicing/fbr_e_invoicing/doctype/fbr_e_inv_setup/fbr_e_inv_setup.js
@@ -3,33 +3,29 @@
 
 frappe.ui.form.on("FBR E-Inv Setup", {
 	refresh(frm) {
-		toggle_fetch_master_data_button(frm);
+		// Keep any existing refresh logic here
 	},
-	after_save(frm) {
-		toggle_fetch_master_data_button(frm);
-	},
+
+	fetch_master_data(frm) {
+		if (!has_required_fbr_credentials(frm)) {
+			frappe.msgprint(__("Please save the API Endpoint and Authorization Token first."));
+			return;
+		}
+
+		frappe.call({
+			method: "fbr_e_invoicing.utils.run_master_data_sync",
+			freeze: true,
+			freeze_message: __("Fetching Master Data..."),
+			callback: () => {
+				frappe.show_alert({ message: __("Master Data Synced Successfully"), indicator: 'green' });
+				frm.reload_doc();
+			}
+		});
+	}
 });
 
 function has_required_fbr_credentials(frm) {
 	const api_endpoint = (frm.doc.api_endpoint || "").trim();
 	const pral_authorization_token = (frm.doc.pral_authorization_token || "").trim();
 	return Boolean(api_endpoint && pral_authorization_token);
-}
-
-function toggle_fetch_master_data_button(frm) {
-	const label = __("Fetch Master Data");
-	frm.page.remove_inner_button(label);
-
-	if (!has_required_fbr_credentials(frm)) {
-		return;
-	}
-
-	frm.page.add_inner_button(label, () => {
-		frappe.call({
-			method: "fbr_e_invoicing.utils.run_master_data_sync",
-			freeze: true,
-			freeze_message: __("Fetching Master Data..."),
-			callback: () => frm.reload_doc(),
-		});
-	});
 }
